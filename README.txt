9022153020662127992f6b6925f0ae2d8b4d96bd
eurekasolution <kjmin@daum.net>
2024-06-10 17:21:21 +0900

    AFin

--- README.txt
+++ README.txt
@@ -7,3 +7,73 @@ B0900 : 두째날 09:00를 의미 (커밋한 시간)
 
 이 README 파일에 필요한 설명을 모아놓을 예정입니다.
 test
+
+
+void test(char *str)
+{
+    char buf[100];
+    strcpy(buf, str);
+}
+// ./kpc "hello world"
+int  main(int argc, char **argv)
+{
+    test(argv[1]);
+
+    int i = 3;
+
+    printf("%03d\n", i); // 003
+}
+
+
+main()
+{
+    a();
+        a1();
+        a2();
+            a21();
+    b();
+        b1();
+            b11();
+        b2();
+
+}
+
+http://localhost:8000
+참고 사이트 
+w3schools.com
+
+
+
+connect()
+for( ;;)
+    insert()
+close();
+
+for(; ; )
+    connect();
+    insert();
+    close();
+
+http://localhost:8000/phpmyadmin
+
+create table first (
+    idx int(10) auto_increment,
+    id  char(20) unique,
+    name char(20) ,
+    birth date,
+    primary key(idx)
+);
+alter database kpc COLLATE='utf8mb4_general_ci';
+drop table first;
+
+
+
+alter table first CONVERT TO 
+    character set utf8mb4 collate utf8mb4_general_ci;
+
+
+insert into first (id, name, birth) 
+    values ('aaaa', '알파벳', '2020-01-01');
+
+
+
--- index.php
+++ index.php
@@ -1,11 +1,37 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <title>Bootstrap 5 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col">AAA</div>
+	</div>
+	<div class="row">
+		<div class="col">BBB</div>
+		<div class="col">CCC</div>
+	</div>
+	<div class="row">
+		<div class="col-2 col-md-6 bg-danger">DDD</div>
+		<div class="col bg-primary">EEE</div>
+	</div>
+	<div class="row">
+		<div class="col-2 col-md-6 bg-danger">FFF</div>
+		<div class="col-8 bg-primary">GGG</div>
+		<div class="col-4 bg-warning">HHH</div>
+	</div>
+	<div class="row">
+		<div class="col-1 d-none col-md-1 d-md-block bg-danger text-white">순서</div>
+		<div class="col-8 col-md-6 bg-primary text-center">제목</div>
+		<div class="col-4 col-md bg-warning text-end">작성자</div>
+		<div class="col d-none col-md d-md-block  bg-secondary">작성일</div>
+	</div>
+</div>
+
+</body>
+</html>
