6b769cd6b2fefb59e858adc072e86dc24fe48f56
eurekasolution <kjmin@daum.net>
2024-06-11 10:45:33 +0900

    B1045

--- index.php
+++ index.php
@@ -7,15 +7,18 @@
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/?family=Material+Icons" />
+
 </head>
 <body>
 <div class="container">
 	<div class="row">
-		<div class="col">AAA</div>
+		<div class="col colLine">AAA</div>
 	</div>
 	<div class="row">
-		<div class="col">BBB</div>
-		<div class="col">CCC</div>
+		<div class="col colLine">BBB</div>
+		<div class="col colLine">CCC</div>
 	</div>
 	<div class="row">
 		<div class="col-2 col-md-6 bg-danger">DDD</div>
@@ -32,7 +35,46 @@
 		<div class="col-4 col-md bg-warning text-end">작성자</div>
 		<div class="col d-none col-md d-md-block  bg-secondary">작성일</div>
 	</div>
-</div>
+
+	<div class="row">
+		<div class="col-2 colLine">아이디</div>
+		<div class="col colLine">
+			<input type="text" name="id" class="form-control" placeholder="아이디 입력">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-2 colLine">비번</div>
+		<div class="col colLine">
+			<input type="password" name="pass" class="form-control" placeholder="아이디 입력">
+		</div>
+	</div>
+
+	
+
+	<div class="row">
+		<div class="col-2 colLine">email</div>
+		<div class="col colLine">
+			<input type="email" name="email" class="form-control" placeholder="아이디 입력">
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-2 colLine">나이</div>
+		<div class="col colLine">
+			<input type="number" name="age" value="10" min="0" max="100" step="2" class="form-control" placeholder="아이디 입력">
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-2 colLine">내용</div>
+		<div class="col colLine">
+			<textarea class="form-control" name="memo" rows=10>안녕하세요?
+홍길동입니다.
+			</textarea>
+		</div>
+	</div>
+
+</div> <!-- container -->
 
 </body>
 </html>
