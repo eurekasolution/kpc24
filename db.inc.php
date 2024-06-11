2c27e20e8326ccb6d86b9b2ed47fa511fbc6c1d9
eurekasolution <kjmin@daum.net>
2024-06-11 11:58:36 +0900

    B1200

--- db.inc.php
+++ db.inc.php
@@ -8,6 +8,7 @@
         $dbPort = "3306";
 
         $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName, $dbPort) or die("Fail : %s " .  $conn->error);
+        $conn->set_charset("utf8mb4");
         return $conn;
     }
 
