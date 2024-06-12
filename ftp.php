5c220e8a1433c29287ee14d5348581bca216f985
eurekasolution <kjmin@daum.net>
2024-06-12 15:41:53 +0900

    C1541

--- ftp.php
+++ ftp.php
@@ -11,4 +11,67 @@
         }
         return $files;
     }
+    function getFiles($path)
+    {
+        $dirHandler = opendir($path);
+        while( ($filename = readdir($dirHandler)) != false)
+        {
+            if(is_dir("$path/$filename"))
+            {
+                
+            }else
+            {
+                $files[] = $filename;
+            }
+        }
+        return $files;
+    }
+
+    $sess_path = "sess_path";
+
+    if(!isset($_SESSION[$sess_path]) or $_SESSION[$sess_path] == "")
+    {
+        $_SESSION[$sess_path] = "./";
+    }
 ?>
+
+<div class="row">
+    <div class="col-3">
+        <table class="table table-bordered">
+            <?php
+                $dirs = getDirs($_SESSION[$sess_path]);
+                $cnt = 0;
+
+                while(isset($dirs[$cnt]))
+                {
+                    echo "
+                    <tr>
+                        <td>$dirs[$cnt]</td>
+                    </tr>
+                    ";
+                    $cnt ++;
+                }
+                
+            ?>
+        </table>
+    </div>
+    <div class="col">
+    <table class="table table-bordered">
+            <?php
+                $files = getFiles($_SESSION[$sess_path]);
+                $cnt = 0;
+
+                while(isset($files[$cnt]))
+                {
+                    echo "
+                    <tr>
+                        <td>$files[$cnt]</td>
+                    </tr>
+                    ";
+                    $cnt ++;
+                }
+                
+            ?>
+        </table>
+    </div>
+</div>
