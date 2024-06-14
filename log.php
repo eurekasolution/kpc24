30cd6f0d4372d615f8d8af818e099ac967395a32
eurekasolution <kjmin@daum.net>
2024-06-14 14:29:21 +0900

    E1430

--- log.php
+++ log.php
@@ -30,12 +30,43 @@
         <div class="col colLine">id</div>
         <div class="col colLine">time</div>
         <div class="col colLine">ip</div>
+        <div class="col colLine">국가</div>
         <div class="col colLine">비고</div>
     </div>
 
     <?php
+        function ip2country($ip)
+        {
+            $splitIP = explode(".", $ip);
+            include "ip_files/" . $splitIP[0] . ".php";
+
+            $code = ($splitIP[0] * 16777216) +($splitIP[1] * 65536) + ($splitIP[2] * 256) + $splitIP[3];
+            foreach($ranges as $key => $value)
+            {
+                if($key <= $code)
+                {
+                    if($ranges[$key][0] >= $code)
+                    {
+                        $country = $ranges[$key][1];
+                        break;
+                    }
+                }
+            }
+
+            if(!isset($country))
+                $country = "";
+
+            if(isset($country) and $country == "")
+                $country = "noflag";
+
+            return $country;
+        }
+
+
         while($data)
         {
+            $nation = ip2country($data["ip"]);
+            $flag = "<img src='flags/$nation.gif'>"
             ?>
             <div class="row">
                 <div class="col colLine"><?php echo $data["idx"]?></div>
@@ -43,6 +74,7 @@
                 <div class="col colLine"><?php echo $data["id"]?></div>
                 <div class="col colLine"><?php echo $data["time"]?></div>
                 <div class="col colLine"><?php echo $data["ip"]?></div>
+                <div class="col colLine"><?php echo $flag ?></div>
                 <div class="col colLine">
                     <button type="button" class="btn btn-primary btn-sm" onClick=confirmDelete(<?php echo $data["idx"]?>)>삭제</button>
                 </div>
